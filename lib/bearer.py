ddda7d2544ad717bb8c28996e36fec89f2061deb
Sara Anguix <sanguix@gmail.com>
2018-05-28 21:22:31 +0100

    Class for credentials, not token.

--- lib/bearer.py
+++ lib/bearer.py
@@ -3,24 +3,24 @@
 
 import base64
 
-class Bearer(object):
+class CredentialForBearer(object):
 
     def __init__(self, key, secret): # constructor
         self.key = key
         self.secret = secret
 
-    def get_token(self):
-        decoded_token = self.get_decoded_token()
-        encoded_token = self.encode_token(decoded_token)
-	return encoded_token
+    def get_credential(self):
+        decoded_credential = self.get_decoded_credential()
+        encoded_credential = self.encode_credential(decoded_credential)
+	return encoded_credential
         
-    def get_decoded_token(self):
+    def get_decoded_credential(self):
         return self.key + ":" + self.secret
 
-    def encode_token(self, string):
+    def encode_credential(self, string):
         return base64.b64encode(string)
         
 if __name__ == "__main__": # if it's executed from console __name__ = true
-    bearer = Bearer("xvz1evFS4wEEPTGEFPHBog", "L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg")
-    assert bearer.get_token() == "eHZ6MWV2RlM0d0VFUFRHRUZQSEJvZzpMOHFxOVBaeVJnNmllS0dFS2hab2xHQzB2SldMdzhpRUo4OERSZHlPZw=="
+    bearer = CredentialForBearer("xvz1evFS4wEEPTGEFPHBog", "L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg")
+    assert bearer.get_credential() == "eHZ6MWV2RlM0d0VFUFRHRUZQSEJvZzpMOHFxOVBaeVJnNmllS0dFS2hab2xHQzB2SldMdzhpRUo4OERSZHlPZw=="
 
